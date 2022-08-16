152a3a5236b370bb9d9c9d7c800efe6be370d17f
kyok01 <kazutakayokoils@gmail.com>
2022-08-16 22:01:12 +0900

    responsive design に対応させる

--- components/FoodVote.jsx
+++ components/FoodVote.jsx
@@ -2,14 +2,14 @@ import { memo } from "react";
 
 const FoodVote = ({ emoji, isPrepared, count, setCount }) => {
   return (
-    <div className="flex flex-wrap justify-center text-2xl">
-      <p className="m-4 w-32">{emoji}</p>
-      <p className="m-4 w-32">{count} 票</p>
+    <div className="flex flex-wrap justify-center text-xl md:text-2xl">
+      <p className="m-4 w-20 md:w-32">{emoji}</p>
+      <p className="m-4 w-20 md:w-32">{count} 票</p>
       {isPrepared ? (
-        <p className="m-4 w-32">準備完了</p>
+        <p className="m-4 w-20 md:w-32">準備完了</p>
       ) : (
         <div
-          className="m-4 w-32"
+          className="m-4 w-20 md:w-32"
           onClick={() => {
             setCount((prevValue) => prevValue + 1);
           }}
